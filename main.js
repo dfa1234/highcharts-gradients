3e920f9bd9d432ff4096df77745d3dda0ed4f91f
dfa1234 <dfaure@outlook.com>
2022-05-08 15:39:26 +0300

    1 file modified

--- main.js
+++ main.js
@@ -2,7 +2,49 @@ const configuration1 = {
   chart: {
     type: 'area',
     spaceTop: 20,
+    events: {
+      render: function () {
+        const chart = this;
+        const moveCursorTo = 'M';
+        const drawLineTo = 'L';
+        const alertPoint = chart.series[0].data[3];
+        const alertPointX = alertPoint.plotX + chart.plotLeft;
+        const alertPointY = alertPoint.plotY + configuration1.chart.spaceTop;
+        const bottomChartY = chart.plotHeight + chart.plotTop;
+
+        const alertLinePath = [
+          [moveCursorTo, alertPointX, alertPointY],
+          [drawLineTo, alertPointX, bottomChartY],
+        ];
+
+        const alertLine = chart.renderer
+          .path(alertLinePath)
+          .attr({
+            'stroke-width': 5,
+            stroke: 'orange',
+            'stroke-dasharray': 3,
+            zIndex: 5,
+          })
+          .add();
+
+        const alertIconCircumference = 50;
+
+        const alertIcon = chart.renderer
+          .image(
+            `https://raw.githubusercontent.com/dfa1234/highcharts-gradients/master/circle.svg`,
+            alertPointX - alertIconCircumference / 2,
+            alertPointY - alertIconCircumference / 2,
+            alertIconCircumference,
+            alertIconCircumference
+          )
+          .attr({
+            zIndex: 6,
+          })
+          .add();
+      },
+    },
   },
+
   title: null,
   credits: {
     enabled: false,
@@ -60,40 +102,3 @@ const configuration1 = {
 ////////////////
 
 const chart = Highcharts.chart('container1', configuration1);
-
-const moveCursorTo = 'M';
-const drawLineTo = 'L';
-const alertPoint = chart.series[0].data[3];
-const alertPointX = alertPoint.plotX + chart.plotLeft;
-const alertPointY = alertPoint.plotY + configuration1.chart.spaceTop;
-const bottomChartY = chart.plotHeight + chart.plotTop;
-
-const alertLinePath = [
-  [moveCursorTo, alertPointX, alertPointY],
-  [drawLineTo, alertPointX, bottomChartY],
-];
-
-const alertLine = chart.renderer
-  .path(alertLinePath)
-  .attr({
-    'stroke-width': 5,
-    stroke: 'green',
-    'stroke-dasharray': 3,
-    zIndex: 5,
-  })
-  .add();
-
-const alertIconCircumference = 20;
-
-const alertIcon = chart.renderer
-  .image(
-    `./circle.svg`,
-    alertPointX - alertIconCircumference / 2,
-    alertPointY - alertIconCircumference / 2,
-    alertIconCircumference,
-    alertIconCircumference
-  )
-  .attr({
-    zIndex: 6,
-  })
-  .add();
