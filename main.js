83d9ab7dca51103c89c7fcad6ac721bf31b9ff68
dfa1234 <dfaure@outlook.com>
2022-05-09 16:37:29 +0300

    2 files modified, 1 added

--- main.js
+++ main.js
@@ -1,55 +1,12 @@
-let svgElements = [];
+import { drawSvg } from './draw-svg';
 
 const configuration1 = {
   chart: {
     type: 'area',
-    spaceTop: 20,
     events: {
-      render: function () {
-        const chart = this;
-
-        svgElements.forEach((element) => element.destroy());
-        svgElements = [];
-
-        const moveCursorTo = 'M';
-        const drawLineTo = 'L';
-        const moonPoint = chart.series[0].data[3];
-        const moonPointX = moonPoint.plotX + chart.plotLeft;
-        const moonPointY = moonPoint.plotY + configuration1.chart.spaceTop / 2;
-        const bottomChartY = chart.plotHeight + chart.plotTop;
-
-        const moonLine = chart.renderer
-          .path([
-            [moveCursorTo, moonPointX, moonPointY],
-            [drawLineTo, moonPointX, bottomChartY],
-          ])
-          .attr({
-            'stroke-width': 5,
-            stroke: 'orange',
-            'stroke-dasharray': 3,
-            zIndex: 3,
-          });
-
-        const moonCircumference = 50;
-        const moonImage = chart.renderer
-          .image(
-            `https://raw.githubusercontent.com/dfa1234/highcharts-gradients/master/circle.svg`,
-            moonPointX - moonCircumference / 2,
-            moonPointY - moonCircumference / 2,
-            moonCircumference,
-            moonCircumference
-          )
-          .attr({
-            zIndex: 4,
-          });
-
-        svgElements.push(moonLine);
-        svgElements.push(moonImage);
-        svgElements.forEach((element) => element.add());
-      },
+      render: drawSvg,
     },
   },
-
   title: null,
   credits: {
     enabled: false,
